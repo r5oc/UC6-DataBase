95b0c61068747e54d0daf5419ce3707e7dab09e2
Gabriel <ofcoliva@gmail.com>
2022-08-03 21:47:43 -0300

    manipulando db
    Arquivos usados para manipular o db

---
+++ 005InserindoLivros.sql
@@ -0,0 +1,14 @@
+USE CLUBE_DO_LIVRO;
+
+
+INSERT INTO LIVROS VALUES
+(3, "O Cortiço", "Aluísio", "Pand Books", "Romance", 47.8),
+(4,"Dom Casmurro", "Machado de Assis", "Via Leitura", "Romance", 48),
+(5, "Memórias Póstumas de Brás Cubás", "Machado de Assis", "Antofágica", "Romance", 44.99),
+(6, "Quincas Borba", "Machado de Assis", "L&PM Editores", "Romance", 48.5),
+(7, "Ícaro", "Gabriel Pedrosa", "Ateliê", "Poesia", 36),
+(8, "Os Lusíadas", "Luíz Vaz de Camões", "Montecristo", "Poesia", 18.79),
+(9, "Outros Jeitos de Usar a Boca", "Rupi Kaur", "Planeta", "Poesia", 34.8),
+(10, "Eu sou Malala", "Malala Yousafzai", "Campahia das letras", "Biografia", 23.99),
+(11, "Minha histíra", "Michelle Obama", "Objetiva", "Biografia", 57.9);
+
